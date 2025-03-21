2ea84f8bed6f7e57436f29e0832455c651992690
Th-Sagar <sagarthapa98122@gmail.com>
2025-03-21 12:47:37 +0545

    feat: router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountListComponent } from './account-list/account-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'accounts',
+    component: AccountListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
